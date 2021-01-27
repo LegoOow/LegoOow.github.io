4b9cc5ce1ed0b3fdc75d38963e08b748d289e763
Anaïs <kevin.castel@protonmail.com>
2021-01-27 15:47:44 +0100

    JS

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-var request = new XMLHttpRequest();
-request.open("GET", "http://localhost:3000/api/teddies");
-request.send();
+function name() {
+
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            let response = JSON.parse(this.responseText);
+            let nameResult = document.getElementById('teddies-name');
+            nameResult.innertHTML = response.0.name;
+            }
+        };
+    request.open("GET", "http://localhost:3000/api/teddies");
+    request.send();
+  };
+  
+  let element = document.getElementById('ask-weather');
+  
+  element.addEventListener('click', function(event) {
+          event.stopPropagation();
+          askWeather();
+  });
+
+
+
+
+
